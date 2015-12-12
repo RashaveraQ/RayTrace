41a5c778074cd00b3eeeb249ef1dab7b8fff9702
RashaveraQ <so_dandy@yahoo.co.jp>
2015-12-12 15:08:18 +0900

    add DevMinus class.

--- src/Minus.cuh
+++ src/Minus.cuh
@@ -1,14 +1,15 @@
 ﻿#pragma once
-#include "Node.cuh"
+#include "Gathering.cuh"
 
-class DevMinus : public DevNode
+class DevMinus : public DevGathering
 {
 public:
 	// コンストラクタ
-	__device__ DevMinus(DevNode* const root = 0, const char* const Name = "Minus", const Sp Color = Sp(255, 255, 255));
-	__device__ DevMinus(const DevMinus& other) : DevNode(other) {}
+	__device__ DevMinus(DevNode* const root = 0, const char* const Name = "Minus") : DevGathering(root, MINUS, Name) {}
+	__device__ DevMinus(const DevMinus& other) : DevGathering(other) {}
 
 	__device__ virtual	bool GetInfo(const Sp& K, const Sp& L, DevInfo& info, const DevInfo* pHint, bool fromOutSide) const;
 	__device__ bool IsInside(const Sp& L) const;
 	__device__ const DevNode* MakeCopy() const { return new DevMinus(*this); }
+	__device__ int cmp_distance(float, float) const;
 };
