26d214dd03c77e76ef2c801f5a2f8660a62803ed
RashaveraQ <so_dandy@yahoo.co.jp>
2015-12-12 15:59:58 +0900

    add DevPolygon2 class.

---
+++ src/Polygon2.cu
@@ -0,0 +1,108 @@
+#include <math.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "DoCuda.h"
+#include "Polygon2.cuh"
+
+__device__
+DevPolygon2::DevPolygon2(const DevPolygon2& other) : DevNode(other)
+{
+	int	i;
+
+	m_N = other.m_N;
+	m_P = new Sp[m_N];
+	for (i = 0; i < m_N; i++)
+		m_P[i] = other.m_P[i];
+}
+
+__device__
+bool DevPolygon2::IsInside(const Sp& L) const
+{
+	if (L.z < 0.0)
+		return false;
+
+	bool	r = false;
+	int		i, j;
+
+	for (i = 0, j = m_N - 1; i < m_N; j = i++)
+	{
+		if ((
+			((m_P[i].y <= L.y) && (L.y < m_P[j].y))
+			||
+			((m_P[j].y <= L.y) && (L.y < m_P[i].y))
+			)
+			&&
+			(
+			L.x < (m_P[j].x - m_P[i].x) * (L.y - m_P[i].y) / (m_P[j].y - m_P[i].y) + m_P[i].x
+			)
+			)
+			r = !r;
+	}
+	return r;
+}
+
+__device__
+bool DevPolygon2::GetInfo(const Sp& K, const Sp& L, DevInfo& info, const DevInfo* pHint, bool fromOutSide) const
+{
+	float	a = K * K;
+	float	b = K * L;
+	float	c = L * L - 1.0f;
+
+	float	bb_ac = b*b - a*c;
+
+	if (bb_ac < 0)
+		return false;
+
+	float	t;
+	float	t1 = (-b + sqrt(bb_ac)) / a;
+	float	t2 = (-b - sqrt(bb_ac)) / a;
+
+	info.isEnter = 0;
+	if (t1 > 0)
+	{
+		if (t2 > 0)
+		{
+			info.isEnter = 1;
+			t = (t1 < t2) ? t1 : t2;
+		}
+		else
+			t = t1;
+	}
+	else
+	{
+		if (t2 > 0)
+			t = t2;
+		else
+			return false;
+	}
+
+	info.Cross = info.Vertical = K * t + L;
+	info.Distance = t * sqrt(K * K);
+	info.Material = m_Material;
+	info.pNode = this;
+
+	return true;
+}
+
+__global__
+void newPolygon2(DevNode** out)
+{
+	if (blockIdx.x == 0 && blockIdx.y == 0 && blockIdx.z == 0 && threadIdx.x == 0 && threadIdx.y == 0 && threadIdx.z == 0)
+		*out = new DevPolygon2();
+}
+
+bool newDevPolygon2(DevNode** out)
+{
+	if (!mallocDev(out))
+		return false;
+
+	newPolygon2<<<1, 1>>>(out);
+
+	// Check for any errors launching the kernel
+	cudaError_t cudaStatus = cudaGetLastError();
+	if (cudaStatus != cudaSuccess) {
+		return false;
+	}
+
+	return true;
+}
--- src/Polygon2.cuh
+++ src/Polygon2.cuh
@@ -3,10 +3,14 @@
 
 class DevPolygon2 : public DevNode
 {
+	int	m_N;
+	Sp	*m_P;
 public:
 	// コンストラクタ
-	__device__ DevPolygon2(DevNode* const root = 0, const char* const Name = "Polygon2", const Sp Color = Sp(255, 255, 255));
-	__device__ DevPolygon2(const DevPolygon2& other) : DevNode(other) {}
+	__device__ DevPolygon2(DevNode* const root = 0, const char* const Name = "Polygon", const Sp Color = Sp(255, 255, 255))
+		: DevNode(root, POLYGON, Name, Color), m_N(0), m_P(0) {}
+
+	__device__ DevPolygon2(const DevPolygon2& other);
 
 	__device__ virtual	bool GetInfo(const Sp& K, const Sp& L, DevInfo& info, const DevInfo* pHint, bool fromOutSide) const;
 	__device__ bool IsInside(const Sp& L) const;
