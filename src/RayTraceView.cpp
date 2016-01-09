a471f89e0d7a1c819c69dbeb58f16d7ff4f0b89e
RashaveraQ <so_dandy@yahoo.co.jp>
2016-01-09 18:29:38 +0900

    refactor.

--- src/RayTraceView.cpp
+++ src/RayTraceView.cpp
@@ -259,9 +259,8 @@ bool CRayTraceView::setManipulatorAxis(const CPoint& point/*, const matrix& mat*
 
 void CRayTraceView::drawManipulator(CDC* pDC) const
 {
-	const Viewport& viewport = m_Viewport;
 	matrix mat;
-	if (!viewport.getManipulatorMatrix(mat))
+	if (!m_Viewport.getManipulatorMatrix(mat))
 		return;
 
 	static COLORREF s_color = RGB(255, 255, 0);
@@ -284,9 +283,9 @@ void CRayTraceView::drawManipulator(CDC* pDC) const
 		r = 50;
 	case eMOVE:
 	case eSCALE:
-		px = p0 + viewport.m_Rotate * sp(0, 0, -2);
-		py = p0 + viewport.m_Rotate * sp(0, -2, 0);
-		pz = p0 + viewport.m_Rotate * sp(-2, 0, 0);
+		px = p0 + m_Viewport.m_Rotate * sp(0, 0, -2);
+		py = p0 + m_Viewport.m_Rotate * sp(0, -2, 0);
+		pz = p0 + m_Viewport.m_Rotate * sp(-2, 0, 0);
 		break;
 	default:
 		px = mat * sp(0, 0, -1);
