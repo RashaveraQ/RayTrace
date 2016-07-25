5ad6b370651036cf048982e03415c336ed83faa6
RashaveraQ <so_dandy@yahoo.co.jp>
2016-07-25 22:52:43 +0900

    fix.

--- src/PolygonSphere.cpp
+++ src/PolygonSphere.cpp
@@ -31,45 +31,45 @@ PolygonSphere::PolygonSphere(Node* const root, const TCHAR* const Name, const sp
 		for (int a = 0; a < SUBDIVISIONS_AXIS - 1; a++) {
 			switch (h) {
 			case 0:
-				m_pFaces[n++] = new Face(m_pVertexes, m_pVertexes + a + 1, m_pVertexes + a);
+				m_pFaces[n++] = new Face(m_pVertexes, getVertex(a+1,1), getVertex(a,1));
 				break;
-			case SUBDIVISIONS_HEIGHT - 1:
-				m_pFaces[n++] = new Face(m_pVertexes + a     + SUBDIVISIONS_AXIS * h,
-										 m_pVertexes + a + 1 + SUBDIVISIONS_AXIS * h,
-										 m_pVertexes + 1     + SUBDIVISIONS_AXIS * (h + 1));
+			case SUBDIVISIONS_HEIGHT - 2:
+				m_pFaces[n++] = new Face(getVertex(a,h), getVertex(a+1,h), getVertex(1,h+1));
 				break;
 			default:
-				m_pFaces[n++] = new Face(m_pVertexes + a     + SUBDIVISIONS_AXIS * h,
-										 m_pVertexes + a + 1 + SUBDIVISIONS_AXIS * h,
-										 m_pVertexes + a     + SUBDIVISIONS_AXIS * (h + 1));
-				m_pFaces[n++] = new Face(m_pVertexes + a     + SUBDIVISIONS_AXIS * (h + 1),
-										 m_pVertexes + a + 1 + SUBDIVISIONS_AXIS * h,
-										 m_pVertexes + a + 1 + SUBDIVISIONS_AXIS * (h + 1));
+				m_pFaces[n++] = new Face(getVertex(a,h), getVertex(a+1,h), getVertex(a,h+1));
+				m_pFaces[n++] = new Face(getVertex(a,h+1), getVertex(a+1,h), getVertex(a+1,h+1));
 				break;
 			}
 		}
 		switch (h) {
 		case 0:
-			m_pFaces[n++] = new Face(m_pVertexes, m_pVertexes + 1, m_pVertexes + SUBDIVISIONS_AXIS);
+			m_pFaces[n++] = new Face(m_pVertexes, m_pVertexes + 1, getVertex(-1,1));
 			break;
 		case SUBDIVISIONS_HEIGHT - 1:
-			m_pFaces[n++] = new Face(m_pVertexes + SUBDIVISIONS_AXIS + SUBDIVISIONS_AXIS * h,
-									 m_pVertexes + 1                 + SUBDIVISIONS_AXIS * h,
-									 m_pVertexes + nV);
+			m_pFaces[n++] = new Face(getVertex(-1,h), getVertex(0,h), getVertex(0,h+1));
 			break;
 		default:
-			m_pFaces[n++] = new Face(m_pVertexes + SUBDIVISIONS_AXIS + SUBDIVISIONS_AXIS * h,
-									 m_pVertexes + 1                 + SUBDIVISIONS_AXIS * h,
-									 m_pVertexes + SUBDIVISIONS_AXIS + SUBDIVISIONS_AXIS * (h + 1));
-			m_pFaces[n++] = new Face(m_pVertexes + SUBDIVISIONS_AXIS + SUBDIVISIONS_AXIS * (h + 1),
-									 m_pVertexes + 1                 + SUBDIVISIONS_AXIS * h,
-									 m_pVertexes + 1                 + SUBDIVISIONS_AXIS * (h + 1));
+			m_pFaces[n++] = new Face(getVertex(-1,h), getVertex(0,h), getVertex(-1,h+1));
+			m_pFaces[n++] = new Face(getVertex(-1,h+1), getVertex(0,h), getVertex(0,h+1));
 			break;
 		}
 	}
 
 	if (!newDeviceNode())
 		exit(1);
+}
+
+Vertex* PolygonSphere::getVertex(int a, int h) const
+{
+	if (h == 0) {
+		return m_pVertexes;
+	}
+	if (0 < h && h < SUBDIVISIONS_HEIGHT - 1) {
+		a = (SUBDIVISIONS_AXIS + a) % SUBDIVISIONS_AXIS;
+		return m_pVertexes + a + SUBDIVISIONS_AXIS * (h - 1);
+	}
+	return m_pVertexes + SUBDIVISIONS_AXIS * (SUBDIVISIONS_HEIGHT - 1) + 1;
 }
 
 PolygonSphere::PolygonSphere(const PolygonSphere& other)
--- src/PolygonSphere.h
+++ src/PolygonSphere.h
@@ -7,6 +7,7 @@ class PolygonSphere : public PolygonPrimitive
 {
 protected:
 	DECLARE_SERIAL(PolygonSphere)
+	Vertex* getVertex(int a, int h) const;
 public:
 	// コンストラクタ
 	PolygonSphere(Node* const root = 0, const TCHAR* const Name = _T("PolygonSphere"), const sp Color = sp(255, 255, 255));
